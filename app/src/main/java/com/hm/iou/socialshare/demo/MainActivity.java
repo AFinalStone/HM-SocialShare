7628bb921921f36372f8b99c822a108e8dc3f47c
houjinyun <houjinyun@gmail.com>
2018-05-10 19:53:27 +0800

    增加分享类型

--- app/src/main/java/com/hm/iou/socialshare/demo/MainActivity.java
+++ app/src/main/java/com/hm/iou/socialshare/demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.hm.iou.socialshare.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.hm.iou.socialshare.ShareManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ShareManager.init(this);
+
+        findViewById(R.id.btn_share_board).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
---
+++ library/src/main/java/com/hm/iou/socialshare/ShareDataTypeEnum.java
@@ -0,0 +1,19 @@
+package com.hm.iou.socialshare;
+
+import java.io.Serializable;
+
+public enum ShareDataTypeEnum implements Serializable {
+    shareAgentIOU("平台借条"), sharePaperBorrowIOU("纸质借条"), sharePaperRecvIOU("纸质收条"),
+    shareFunIOU("娱乐借条"), shareMoneyIOU("资金借条"), sharePrepareToCreateMoneyIOU("准备资料"),
+    shareMyCard("个人名片"), shareUrl("分享网页链接");
+
+    String value;
+
+    ShareDataTypeEnum(String value) {
+        this.value = value;
+    }
+
+    public String getTypeName() {
+        return value;
+    }
+}
