d56618f182897630052f4ba164b2a8f42c9ab18d
houjinyun <houjinyun@gmail.com>
2018-07-09 19:57:59 +0800

    修改读取metadata错误的问题

--- library/src/main/java/com/hm/iou/socialshare/SocialShareAppLike.java
+++ library/src/main/java/com/hm/iou/socialshare/SocialShareAppLike.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
 
 import com.umeng.socialize.PlatformConfig;
 
@@ -39,9 +41,37 @@ public class SocialShareAppLike {
         //友盟推送也需要初始化，在这里暂时移除掉，在主工程里初始化
         //UMConfigure.init(context, UMConfigure.DEVICE_TYPE_PHONE, "");
 
-        PlatformConfig.setWeixin(getMetaData(context, "WEIXIN_ID"), getMetaData(context, "WEIXIN_SECRET"));
-        PlatformConfig.setQQZone(getMetaData(context, "TENCENT_QQ_ID"), getMetaData(context, "TENCENT_QQ_SECRET"));
-        PlatformConfig.setSinaWeibo(getMetaData(context, "WEIBO_ID"), getMetaData(context, "WEIBO_SECRET"), getMetaData(context, "WEIBO_CALLBACK_URL"));
+        String wxId = null;
+        String wxSecret = null;
+        String qqId = null;
+        String qqSecret = null;
+        String weiboId = null;
+        String weiboSecret = null;
+        String weiboCallback = null;
+        try {
+            ApplicationInfo info = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle data = info.metaData;
+            wxId = data.getString("WEIXIN_ID");
+            wxSecret = data.getString("WEIXIN_SECRET");
+            qqId = data.getString("TENCENT_QQ_ID");
+            if (TextUtils.isEmpty(qqId)) {
+                qqId = data.getInt("TENCENT_QQ_ID") + "";
+            }
+            qqSecret = data.getString("TENCENT_QQ_SECRET");
+            weiboId = data.getString("WEIBO_ID");
+            if (TextUtils.isEmpty(weiboId)) {
+                weiboId = data.getInt("WEIBO_ID") + "";
+            }
+            weiboSecret = data.getString("WEIBO_SECRET");
+            weiboCallback = data.getString("WEIBO_CALLBACK_URL");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        PlatformConfig.setWeixin(wxId, wxSecret);
+        PlatformConfig.setQQZone(qqId, qqSecret);
+        PlatformConfig.setSinaWeibo(weiboId, weiboSecret, weiboCallback);
     }
 
     private String getMetaData(Context context, String key) {
