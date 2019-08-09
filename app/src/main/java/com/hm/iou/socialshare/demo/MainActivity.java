b073667e29effd88d0884d1694c49fe87498cabc
石磊 <>
2019-08-09 09:42:45 +0800

    为demo添加支持kotlin的依赖

--- app/src/main/java/com/hm/iou/socialshare/demo/MainActivity.java
+++ app/src/main/java/com/hm/iou/socialshare/demo/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 new SharePlatformDialog
                         .Builder(MainActivity.this)
                         .setPicUrl("https://upload-images.jianshu.io/upload_images/972352-9911637db5512613.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/700")
+                        .setShowImage(true)
                         .setPlatforms(listData)
                         .show();
             }
